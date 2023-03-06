b4007a0619dbd652913e10115344618b292c9fa3
Daniel Oloruntoba <danieloloruntoba681@gmail.com>
2023-03-06 21:20:38 +0100

    issue: textfield

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,7 +6,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
+      backgroundColor: Colors.white,
       // appBar: AppBar(
       //   title: Column(
       //     crossAxisAlignment: CrossAxisAlignment.start,
@@ -27,9 +27,43 @@ class HomePage extends StatelessWidget {
       //   backgroundColor: Colors.white,
       //   elevation: 0,
       // ),
-      body: Row(
-        children: const [Text("John Doe"), Icon(Icons.card_travel_outlined)],
-      ),
+      body: Stack(children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 90, left: 25, right: 25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        "Good Morning,",
+                        style: TextStyle(fontSize: 17),
+                      ),
+                      Text(
+                        "John Doe",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  const Icon(Icons.person, size: 40),
+                ],
+              ),
+              const TextField(
+                decoration: InputDecoration(
+                    labelText: "Search here...",
+                    prefixIcon: Icon(Icons.search),
+                    hoverColor: Colors.grey,
+                    suffixIcon: Icon(Icons.sync_alt_outlined)),
+              )
+            ],
+          ),
+        ),
+      ]),
     );
   }
 }
