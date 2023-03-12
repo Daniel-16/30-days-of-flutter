aed8273a757d5a962d175011a8ca6d4f7b8e047d
Daniel Oloruntoba <danieloloruntoba681@gmail.com>
2023-03-13 00:11:32 +0100

    added list cards

--- lib/pages/favorite_screen.dart
+++ lib/pages/favorite_screen.dart
@@ -22,60 +22,297 @@ class FavoriteScreen extends StatelessWidget {
               color: Colors.black,
             )),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.only(left: 16),
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: const Text(
-                "My Favorites",
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(left: 16),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "My Favorites",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 150,
-            child: Card(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15)),
-              child: Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8),
-                    child: SizedBox(
-                        width: 100,
-                        height: 100,
-                        child: ClipRRect(
-                            borderRadius: BorderRadius.circular(15),
-                            child: Image.asset(
-                              "assets/food3.jpg",
-                              fit: BoxFit.fill,
-                            ))),
+            SizedBox(
+              height: 150,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8),
+                        child: SizedBox(
+                            width: 100,
+                            height: 100,
+                            child: ClipRRect(
+                                borderRadius: BorderRadius.circular(15),
+                                child: Image.asset(
+                                  "assets/food3.jpg",
+                                  fit: BoxFit.cover,
+                                ))),
+                      ),
+                      SizedBox(
+                          width: 250,
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                left: 8, right: 8, top: 40),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Text(
+                                  "Duis ipsum id labore sint aliqua.",
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  "Est dolore adipisicing eu ea excepteur sint qui ullamco non proident eu commodo Lorem sit.",
+                                  style: TextStyle(
+                                      fontSize: 12, color: Colors.grey),
+                                ),
+                              ],
+                            ),
+                          ))
+                    ],
                   ),
-                  SizedBox(
-                      width: 200,
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 8, right: 8),
-                        child: Column(
-                          children: const [
-                            Text(
-                              "Duis ipsum id labore sint aliqua.",
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            Text(
-                              "Est dolore adipisicing eu ea excepteur sint qui ullamco non proident eu commodo Lorem sit.",
-                              style:
-                                  TextStyle(fontSize: 12, color: Colors.grey),
-                            ),
-                          ],
-                        ),
-                      ))
-                ],
+                ),
               ),
             ),
-          )
-        ],
+            SizedBox(
+              height: 150,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8),
+                        child: SizedBox(
+                            width: 100,
+                            height: 100,
+                            child: ClipRRect(
+                                borderRadius: BorderRadius.circular(15),
+                                child: Image.asset(
+                                  "assets/food2.jpg",
+                                  fit: BoxFit.cover,
+                                ))),
+                      ),
+                      SizedBox(
+                          width: 250,
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                left: 8, right: 8, top: 40),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Text(
+                                  "Duis ipsum id labore sint aliqua.",
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  "Est dolore adipisicing eu ea excepteur sint qui ullamco non proident eu commodo Lorem sit.",
+                                  style: TextStyle(
+                                      fontSize: 12, color: Colors.grey),
+                                ),
+                              ],
+                            ),
+                          ))
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 150,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8),
+                        child: SizedBox(
+                            width: 100,
+                            height: 100,
+                            child: ClipRRect(
+                                borderRadius: BorderRadius.circular(15),
+                                child: Image.asset(
+                                  "assets/food1.jpg",
+                                  fit: BoxFit.cover,
+                                ))),
+                      ),
+                      SizedBox(
+                          width: 250,
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                left: 8, right: 8, top: 40),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Text(
+                                  "Duis ipsum id labore sint aliqua.",
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  "Est dolore adipisicing eu ea excepteur sint qui ullamco non proident eu commodo Lorem sit.",
+                                  style: TextStyle(
+                                      fontSize: 12, color: Colors.grey),
+                                ),
+                              ],
+                            ),
+                          ))
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 150,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8),
+                        child: SizedBox(
+                            width: 100,
+                            height: 100,
+                            child: ClipRRect(
+                                borderRadius: BorderRadius.circular(15),
+                                child: Image.asset(
+                                  "assets/food2.jpg",
+                                  fit: BoxFit.cover,
+                                ))),
+                      ),
+                      SizedBox(
+                          width: 250,
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                left: 8, right: 8, top: 40),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Text(
+                                  "Duis ipsum id labore sint aliqua.",
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  "Est dolore adipisicing eu ea excepteur sint qui ullamco non proident eu commodo Lorem sit.",
+                                  style: TextStyle(
+                                      fontSize: 12, color: Colors.grey),
+                                ),
+                              ],
+                            ),
+                          ))
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 150,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8),
+                        child: SizedBox(
+                            width: 100,
+                            height: 100,
+                            child: ClipRRect(
+                                borderRadius: BorderRadius.circular(15),
+                                child: Image.asset(
+                                  "assets/food1.jpg",
+                                  fit: BoxFit.cover,
+                                ))),
+                      ),
+                      SizedBox(
+                          width: 250,
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                left: 8, right: 8, top: 40),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Text(
+                                  "Duis ipsum id labore sint aliqua.",
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  "Est dolore adipisicing eu ea excepteur sint qui ullamco non proident eu commodo Lorem sit.",
+                                  style: TextStyle(
+                                      fontSize: 12, color: Colors.grey),
+                                ),
+                              ],
+                            ),
+                          ))
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 150,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8),
+                        child: SizedBox(
+                            width: 100,
+                            height: 100,
+                            child: ClipRRect(
+                                borderRadius: BorderRadius.circular(15),
+                                child: Image.asset(
+                                  "assets/food3.jpg",
+                                  fit: BoxFit.cover,
+                                ))),
+                      ),
+                      SizedBox(
+                          width: 250,
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                left: 8, right: 8, top: 40),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Text(
+                                  "Duis ipsum id labore sint aliqua.",
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  "Est dolore adipisicing eu ea excepteur sint qui ullamco non proident eu commodo Lorem sit.",
+                                  style: TextStyle(
+                                      fontSize: 12, color: Colors.grey),
+                                ),
+                              ],
+                            ),
+                          ))
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
