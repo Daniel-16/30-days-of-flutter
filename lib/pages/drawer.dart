9a7efeefb08c7a5976084e0e41e5ecb667be9c2d
Daniel Oloruntoba <danieloloruntoba681@gmail.com>
2023-03-14 23:31:40 +0100

    feat: list tiles in drawer

--- lib/pages/drawer.dart
+++ lib/pages/drawer.dart
@@ -7,19 +7,47 @@ class DrawerWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       width: 300,
-      child: ListView(
-        children: const [
-          DrawerHeader(child: Text("Settings")),
-          ListTile(
-            title: Text("Widget"),
-          ),
-          ListTile(
-            title: Text("Widget"),
-          ),
-          ListTile(
-            title: Text("Widget"),
-          ),
-        ],
+      child: Padding(
+        padding: const EdgeInsets.only(top: 8.0),
+        child: ListView(
+          children: [
+            Image.asset(
+              "assets/hack.jpg",
+            ),
+            const ListTile(
+              title: Text("Shared with me"),
+              leading: Icon(Icons.group),
+            ),
+            const ListTile(
+              title: Text("Starred"),
+              leading: Icon(Icons.star),
+            ),
+            const ListTile(
+              title: Text("Recent"),
+              leading: Icon(Icons.access_time),
+            ),
+            const ListTile(
+              title: Text("Offline"),
+              leading: Icon(Icons.download_done),
+            ),
+            const ListTile(
+              title: Text("Uploads"),
+              leading: Icon(Icons.upload),
+            ),
+            const ListTile(
+              title: Text("Backups"),
+              leading: Icon(Icons.cloud_upload),
+            ),
+            const ListTile(
+              title: Text("Trash"),
+              leading: Icon(Icons.delete),
+            ),
+            const ListTile(
+              title: Text("Settings & Account"),
+              leading: Icon(Icons.settings),
+            ),
+          ],
+        ),
       ),
     );
   }
