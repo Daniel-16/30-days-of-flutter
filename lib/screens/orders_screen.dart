d0452bede00cb47f164b71456e3849d71b312752
Daniel Oloruntoba <danieloloruntoba681@gmail.com>
2023-03-21 19:58:06 +0100

    feat: listview for image

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_practice/screens/test.dart';
 
 class OrdersScreen extends StatelessWidget {
   const OrdersScreen({super.key});
@@ -20,207 +21,193 @@ class OrdersScreen extends StatelessWidget {
         ],
         iconTheme: const IconThemeData(color: Colors.black),
         leading: IconButton(
-          onPressed: () => {},
+          onPressed: () => {Navigator.popAndPushNamed(context, '/')},
           icon: const Icon(Icons.arrow_back_ios),
         ),
       ),
       body: SingleChildScrollView(
         child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              children: [
-                SizedBox(
-                  width: 350,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 16, left: 16),
+                child: Container(
                   height: 280,
-                  child: SingleChildScrollView(
+                  child: ListView.builder(
                     scrollDirection: Axis.horizontal,
-                    child: Row(
-                      children: [
-                        ClipRRect(
+                    shrinkWrap: true,
+                    itemCount: 3,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        width: 200,
+                        height: 300,
+                        margin: EdgeInsets.only(right: 10),
+                        decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(15),
-                            child: Image.asset(
-                              "assets/food1.jpg",
-                              fit: BoxFit.fill,
-                            )),
-                        const SizedBox(
-                          width: 30,
-                        ),
-                        ClipRRect(
-                            borderRadius: BorderRadius.circular(15),
-                            child: Image.asset(
-                              "assets/food2.jpg",
-                              fit: BoxFit.fill,
-                            )),
-                        const SizedBox(
-                          width: 30,
-                        ),
-                        ClipRRect(
-                            borderRadius: BorderRadius.circular(15),
-                            child: Image.asset(
-                              "assets/food3.jpg",
-                              fit: BoxFit.fill,
-                            )),
-                      ],
-                    ),
+                            image: DecorationImage(
+                                image:
+                                    AssetImage("assets/food${index + 1}.jpg"),
+                                fit: BoxFit.cover)),
+                      );
+                    },
                   ),
                 ),
-                const Padding(
-                  padding: EdgeInsets.only(top: 10),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Country Ranged Meat",
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 10, right: 16, left: 16),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Country Ranged Meat",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10),
+                        child: Text(
+                          "Est sit aute excepteur in sint sit ad eu non. Veniam irure officia ea id non sint do nisi velit aliquip nulla culpa consequat.Aliquip anim eu exercitation cillum deserunt elit dolore ut pariatur labore. Sunt voluptate commodo magna quis nisi commodo elit. Consectetur esse exercitation ullamco ea non cupidatat officia amet consequat. Occaecat reprehenderit aute esse id in aute eu non enim id voluptate aliqua dolor. Occaecat ullamco do dolor cupidatat laboris dolor .",
                           style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold),
+                              fontSize: 15,
+                              color: Color.fromARGB(255, 129, 129, 129)),
                         ),
-                        Padding(
-                          padding: EdgeInsets.only(top: 10),
-                          child: Text(
-                            "Est sit aute excepteur in sint sit ad eu non. Veniam irure officia ea id non sint do nisi velit aliquip nulla culpa consequat.Aliquip anim eu exercitation cillum deserunt elit dolore ut pariatur labore. Sunt voluptate commodo magna quis nisi commodo elit. Consectetur esse exercitation ullamco ea non cupidatat officia amet consequat. Occaecat reprehenderit aute esse id in aute eu non enim id voluptate aliqua dolor. Occaecat ullamco do dolor cupidatat laboris dolor .",
-                            style: TextStyle(
-                                fontSize: 15,
-                                color: Color.fromARGB(255, 129, 129, 129)),
-                          ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 5),
+                        child: Row(
+                          children: [
+                            Text(
+                              "Reviews",
+                              style: TextStyle(
+                                  fontSize: 15, fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              "(120)",
+                              style: TextStyle(fontSize: 15),
+                            ),
+                            SizedBox(
+                              width: 110,
+                            ),
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.star,
+                                  size: 18,
+                                  color: Colors.deepOrangeAccent,
+                                ),
+                                Icon(
+                                  Icons.star,
+                                  size: 18,
+                                  color: Colors.deepOrangeAccent,
+                                ),
+                                Icon(
+                                  Icons.star,
+                                  size: 18,
+                                  color: Colors.deepOrangeAccent,
+                                ),
+                                Icon(
+                                  Icons.star_half,
+                                  size: 18,
+                                  color: Colors.deepOrangeAccent,
+                                ),
+                                Icon(
+                                  Icons.star_border,
+                                  size: 18,
+                                  color: Colors.deepOrangeAccent,
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  "4.2",
+                                  style: TextStyle(fontSize: 15),
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Icon(
+                                  Icons.arrow_forward_ios,
+                                  size: 18,
+                                )
+                              ],
+                            )
+                          ],
                         ),
-                        Padding(
-                          padding: EdgeInsets.only(top: 5),
-                          child: Row(
-                            children: [
-                              Text(
-                                "Reviews",
-                                style: TextStyle(
-                                    fontSize: 15, fontWeight: FontWeight.bold),
-                              ),
-                              Text(
-                                "(120)",
-                                style: TextStyle(fontSize: 15),
-                              ),
-                              SizedBox(
-                                width: 110,
-                              ),
-                              Row(
-                                children: [
-                                  Icon(
-                                    Icons.star,
-                                    size: 18,
-                                    color: Colors.deepOrangeAccent,
-                                  ),
-                                  Icon(
-                                    Icons.star,
-                                    size: 18,
-                                    color: Colors.deepOrangeAccent,
-                                  ),
-                                  Icon(
-                                    Icons.star,
-                                    size: 18,
-                                    color: Colors.deepOrangeAccent,
-                                  ),
-                                  Icon(
-                                    Icons.star_half,
-                                    size: 18,
-                                    color: Colors.deepOrangeAccent,
-                                  ),
-                                  Icon(
-                                    Icons.star_border,
-                                    size: 18,
-                                    color: Colors.deepOrangeAccent,
-                                  ),
-                                  SizedBox(
-                                    width: 10,
-                                  ),
-                                  Text(
-                                    "4.2",
-                                    style: TextStyle(fontSize: 15),
-                                  ),
-                                  SizedBox(
-                                    width: 10,
-                                  ),
-                                  Icon(
-                                    Icons.arrow_forward_ios,
-                                    size: 18,
-                                  )
-                                ],
-                              )
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
-                const SizedBox(
-                  height: 30,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      height: 50,
-                      width: 150,
-                      child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              elevation: 0,
-                              backgroundColor: Colors.white,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(13),
-                                  side: const BorderSide(color: Colors.black))),
-                          onPressed: () => {},
-                          child: const Text(
-                            "Add to Cart",
-                            style: TextStyle(color: Colors.black),
-                          )),
-                    ),
-                    const SizedBox(width: 20),
-                    SizedBox(
-                      height: 50,
-                      width: 150,
-                      child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              elevation: 0,
-                              backgroundColor: Colors.deepOrangeAccent,
-                              shape: RoundedRectangleBorder(
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 50,
+                    width: 150,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            elevation: 0,
+                            backgroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
                                 borderRadius: BorderRadius.circular(13),
-                              )),
-                          onPressed: () => {
-                                showDialog(
-                                    context: context,
-                                    builder: (BuildContext context) {
-                                      return AlertDialog(
-                                        title:
-                                            const Text("Checkout listed items"),
-                                        content: const Text(
-                                            "Are you sure you want to checkout?"),
-                                        actions: [
-                                          TextButton(
-                                              style: TextButton.styleFrom(
-                                                  backgroundColor:
-                                                      Colors.deepOrangeAccent),
-                                              onPressed: () {
-                                                Navigator.of(context)
-                                                    .pop("Yes");
-                                              },
-                                              child: const Text(
-                                                "Yes",
-                                                style: TextStyle(
-                                                    color: Colors.white),
-                                              ))
-                                        ],
-                                      );
-                                    })
-                              },
-                          child: const Text(
-                            "Checkout",
-                            style: TextStyle(color: Colors.white),
-                          )),
-                    ),
-                  ],
-                )
-              ],
-            ),
+                                side: const BorderSide(color: Colors.black))),
+                        onPressed: () =>
+                            {Navigator.pushNamed(context, '/test')},
+                        child: const Text(
+                          "Add to Cart",
+                          style: TextStyle(color: Colors.black),
+                        )),
+                  ),
+                  const SizedBox(width: 20),
+                  SizedBox(
+                    height: 50,
+                    width: 150,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            elevation: 0,
+                            backgroundColor: Colors.deepOrangeAccent,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(13),
+                            )),
+                        onPressed: () => {
+                              showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return AlertDialog(
+                                      title:
+                                          const Text("Checkout listed items"),
+                                      content: const Text(
+                                          "Are you sure you want to checkout?"),
+                                      actions: [
+                                        TextButton(
+                                            style: TextButton.styleFrom(
+                                                backgroundColor:
+                                                    Colors.deepOrangeAccent),
+                                            onPressed: () {
+                                              Navigator.of(context).pop("Yes");
+                                            },
+                                            child: const Text(
+                                              "Yes",
+                                              style: TextStyle(
+                                                  color: Colors.white),
+                                            ))
+                                      ],
+                                    );
+                                  })
+                            },
+                        child: const Text(
+                          "Checkout",
+                          style: TextStyle(color: Colors.white),
+                        )),
+                  ),
+                ],
+              )
+            ],
           ),
         ),
       ),
