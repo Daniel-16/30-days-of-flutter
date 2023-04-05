c4c3ad94499a87914d5742ffc848ee0fe41b4b87
Daniel Oloruntoba <danieloloruntoba681@gmail.com>
2023-04-05 17:20:55 +0100

    search functionality

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:easy_search_bar/easy_search_bar.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -8,6 +13,26 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  TextEditingController controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    getNewsInformation();
+  }
+
+  Future<void> getNewsInformation() async {
+    await dotenv.load();
+    String apiKey = dotenv.env['API_KEY'].toString();
+    try {
+      final response = await http.get(
+          Uri.parse('https://newsapi.org/v2/everything?q=Tech&apiKey=$apiKey'));
+      final data = jsonDecode(response.body);
+    } catch (error) {
+      print(error);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,14 +51,20 @@ class _SearchScreenState extends State<SearchScreen> {
       ),
       body: Container(
         padding: const EdgeInsets.all(8),
-        child: TextField(
-          keyboardType: TextInputType.text,
-          decoration: InputDecoration(
-              hintText: "Search for news here",
-              prefixIcon: const Icon(Icons.search),
-              border: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.grey),
-                  borderRadius: BorderRadius.circular(15))),
+        child: Column(
+          children: [
+            TextField(
+              controller: controller,
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                  hintText: "Search for news here",
+                  prefixIcon: const Icon(Icons.search),
+                  suffixIcon: Icon(Icons.cancel_outlined),
+                  border: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(15))),
+            ),
+          ],
         ),
       ),
     );
